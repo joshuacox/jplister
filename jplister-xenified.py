d76964211350e7dd4d1184c56009b1500f1ac5da
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2024-10-12 10:38:36 -0500

    test

---
+++ jplister-xenified.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from typing import List, TypeVar
+T = TypeVar("T")
+def first(container: List[T]) -> T:
+    return container[0]
+list_one: List[str] = ["a", "b", "c"]
+print(first(list_one))
+list_two: List[int] = [1, 2, 3]
+print(first(list_two))
---
+++ test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+badList = [ 1, "one" ]
+if type(badList[1]) == str:
+    print('is string')
